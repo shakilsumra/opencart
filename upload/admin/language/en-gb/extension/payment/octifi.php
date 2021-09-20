1942df422545a6cb15808a02792d204eb78574a8
shakilsumra <sumra_mca@yahoo.co.in>
2021-09-20 19:35:52 +0530

    Create octifi.php

---
+++ upload/admin/language/en-gb/extension/payment/octifi.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']      = 'OctiFi Payment Method';
+
+// Text
+$_['text_extension']     = 'Extensions';
+$_['text_success']       = 'Success: You have modified Cash On Delivery payment module!';
+$_['text_edit']          = 'Edit Cash On Delivery';
+$_['text_octifi']		 = '<img src="view/image/payment/ocifi.png" alt="Paymate" title="Paymate" style="border: 1px solid #EEEEEE;height: 51px;width: 150px;" />';
+// Entry
+$_['entry_total']        = 'Minimum Total';
+$_['entry_order_status'] = 'Order Status';
+$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order';
+$_['entry_public_key']   = 'Public Key';
+$_['entry_private_key']   = 'Private Key';
+// Help
+$_['help_total']         = 'The checkout total the order must reach before this payment method becomes active.';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify payment Cash On Delivery!';
